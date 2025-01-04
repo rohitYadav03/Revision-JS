d54e7e6626f2dd4050ff6f857e1a81ed4d89deb9
Rohit Yadav <rohityadav85801@gmail.com>
2025-01-04 11:07:40 +0000

    Initial commit

---
+++ JavaScript-Revision/basic.js
@@ -0,0 +1,61 @@
+// Question 1: Basic Function - Sum of Two Numbers
+
+// function sum (a,b) {
+// return a + b ;
+// };
+// let result = sum(5,6);
+// console.log(result);
+
+// Question 2: Even or Odd Number
+
+// function EvenOrOdd(num) {
+//   if(num%2===0) {
+//     console.log("Even Number:",num);
+//   }
+//   else{
+//     console.log("Odd Number:",num);
+    
+//   }
+// }
+// EvenOrOdd(6);
+
+// Question 3: Factorial of a Number
+
+
+// function Factorial (Number) {
+//     let result = 1;
+//     for(let i = 1; i<= Number; i++){
+//         result = result * i;
+//     }
+//     return result;
+// } 
+// let finalRes = Factorial(5);
+// console.log(finalRes);
+// // console.log(result);
+
+// console.log(Factorial(4));
+// console.log(result);
+
+// Question 4: Find if a Number Exists in an Array
+
+// let arr = [1,2,3,45,56,4,5,6,7,8];
+// let num = 7;
+// function checkNumber (arr, num) {
+//    for(let i =0 ; i< arr.length; i++) {
+//     if(arr[i] === num) {
+//         return true;
+//     }
+//    }
+//    return false;
+// }
+// let finalAns = checkNumber(arr,num);
+// console.log(finalAns);
+
+// Question 5: Print Numbers from 1 to 10 using a While Loop
+// let Number = 1;
+// while (Number <= 10){
+  
+//     console.log(Number);
+//     Number++
+// }
+// // console.log(Number);
