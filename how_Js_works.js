806d06be24c91c229220cf12e742ec4558ff4144
Rohit Yadav <rohityadav85801@gmail.com>
2025-01-12 11:10:23 +0000

    How Js internally work

---
+++ how_Js_works.js
@@ -0,0 +1,14 @@
+console.log("hello");
+setTimeout(function(){
+    console.log("inside set timeout");
+    
+},0)
+console.log("hello2");
+console.log("hello3");
+console.log("hello4");
+for(let i =0; i<1000000; i++){
+
+}
+console.log("end");
+
+
